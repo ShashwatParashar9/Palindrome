99fe482a1efa4072758959c55903d96d97b76672
shashwat <gshashwatparashar@gmail.com>
2026-02-21 13:40:37 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // Display system welcome messages
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        Scanner scanner = new Scanner(System.in);
 
+        // Read input text
+        System.out.print("Input text: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
+
+        scanner.close();
     }
 }
